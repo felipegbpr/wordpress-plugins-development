55022c55e526dd59a0638a9ced5aa760d89212eb
Felipe Borges <felipetecacc@gmail.com>
2025-03-22 12:00:04 -0300

    config: Structuring the plugin

---
+++ plugins/creative-slider/creative-slider.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Plugin Name: Creative Slider
+ * Plugin URI: https://www.wordpress.org/creative-slider
+ * Description: My plugin's description 
+ * Version: 1.0
+ * Requires at latest: 5.6
+ * Author: Felipe Borges
+ * Author URI: https://www.authorwp.net 
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: squeeze-slider
+ * Domain Path: /languages
+ */
+
+/*
+Creative Slider is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Creative Slider is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Creative Slider. If not, see {URI to Plugin License}.
+*/
+
+if ( !defined( 'ABSPATH' ) ) {
+  exit;
+}
