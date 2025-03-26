d2c86e597ea4affdbf875d49608a2a0ccc560b15
Felipe Borges <felipetecacc@gmail.com>
2025-03-25 21:37:48 -0300

    impl: API Metabox - Validating and sanitizing information

---
+++ creative-slider/creative-slider.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Plugin Name: Creative Slider
+ * Plugin URI: https://www.wordpress.org/creative-slider
+ * Description: My plugin's description 
+ * Version: 1.0
+ * Requires at latest: 5.6
+ * Author: Felipe Borges
+ * Author URI: https://www.authorwp.net 
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: squeeze-slider
+ * Domain Path: /languages
+ */
+
+/*
+Creative Slider is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Creative Slider is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Creative Slider. If not, see {URI to Plugin License}.
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
+
+if ( ! class_exists( 'Creative_Slider' ) ) {
+  class Creative_Slider {
+    function __construct() {
+      $this->define_constants();
+
+      require_once( CREATIVE_SLIDER_PATH . 'post-types/class.creative-slider-cpt.php' );
+      $Creative_Slider_Post_Type = new Creative_Slider_Post_Type();
+    }    
+
+    public function define_constants() {
+      define( 'CREATIVE_SLIDER_PATH', plugin_dir_path( __FILE__ ) );
+      define( 'CREATIVE_SLIDER_URL', plugin_dir_url( __FILE__ ) );
+      define( 'CREATIVE_VERSION', '1.0.0' );
+    }
+
+    public static function activate() {
+      update_option( 'rewrite_rules', '' );
+    }
+
+    public static function deactivate() {
+      flush_rewrite_rules();
+      unregister_post_type( 'creative-slider' );
+    }
+
+    public static function uninstall() {
+
+    }
+
+  }
+}
+
+if ( class_exists( 'Creative_Slider' ) ) {
+  register_activation_hook( __FILE__, array( 'Creative_Slider', 'activate' ) );
+  register_deactivation_hook( __FILE__, array( 'Creative_Slider', 'deactivate' ) );
+  register_uninstall_hook( __FILE__, array( 'Creative_Slider', 'uninstall' ) );
+
+  $creative_slider = new Creative_Slider();
+}
---
+++ creative-slider/post-types/class.creative-slider-cpt.php
@@ -0,0 +1,77 @@
+<?php
+
+if ( !class_exists( 'Creative_Slider_Post_Type' ) ) {
+  class Creative_Slider_Post_Type {
+    function __construct() {
+      add_action( 'init', array( $this, 'create_post_type' ));
+      add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ));
+      add_action( 'save_post', array( $this, 'save_post' ), 10, 2);
+    }
+
+    public function create_post_type() {
+      register_post_type(
+        'creative-slider',
+        array(
+          'label' => 'Slider',
+          'description' => 'Sliders',
+          'labels' => array(
+            'name' => 'Sliders',
+            'singular_name' => 'Slider'
+          ),
+          'public'   => true,
+          'supports' => array( 'title', 'editor', 'thumbnail', ),
+          'hierarchical' => false,
+          'show_ui' => true,
+          'show_in_menu'  => true,
+          'menu_position' => 5,
+          'show_in_admin_bar' => true,
+          'show_in_admin_bar' => true,
+          'show_in_nav_menus' => true,
+          'can_export'    => true,
+          'has_archive'   => false,
+          'exclude_from_search' => false,
+          'publicly_queryable'  => true,
+          'show_in_rest' => true,
+          'menu_icon' => 'dashicons-images-alt2',
+          // 'register_meta_box_cb' => array( $this, 'add_meta_boxes' )
+        )
+      );
+    }
+
+    public function add_meta_boxes() {
+      add_meta_box(
+        'creative_slider_meta_box',
+        'Link Options',
+        array( $this, 'add_inner_meta_boxes' ),
+        'creative-slider',
+        'normal',
+        'high'
+      );
+    }
+
+    public function add_inner_meta_boxes( $post ) {
+      require_once( CREATIVE_SLIDER_PATH . 'views/creative-slider_metabox.php' );
+    }
+
+    public function save_post( $post_id ){
+      if ( isset( $_POST['action'] ) && $_POST['action'] == 'editpost' ){
+          $old_link_text = get_post_meta( $post_id, 'creative_slider_link_text', true );
+          $new_link_text = $_POST['creative_slider_link_text'];
+          $old_link_url = get_post_meta( $post_id, 'creative_slider_link_url', true );
+          $new_link_url = $_POST['creative_slider_link_url'];
+
+          if ( empty( $new_link_text ) ) {
+            update_post_meta( $post_id, 'creative_slider_link_text', 'Add some text');
+          } else {
+            update_post_meta( $post_id, 'creative_slider_link_text', sanitize_text_field( $new_link_text ), $old_link_text );
+          } 
+
+          if ( empty( $new_link_url ) ) {
+            update_post_meta( $post_id, 'creative_slider_link_url', '#');
+          } else {
+            update_post_meta( $post_id, 'creative_slider_link_url', esc_url_raw( $new_link_url ), $old_link_url );
+          }
+      }
+     }
+  }
+}
