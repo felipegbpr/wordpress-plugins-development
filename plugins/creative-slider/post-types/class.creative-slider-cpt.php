edbe44bd5f11800734410048f829f9dc8b84eb47
Felipe Borges <felipetecacc@gmail.com>
2025-04-01 23:48:14 -0300

    impl: Shortcode API - Creating a shortcode view (part 3)

--- plugins/creative-slider/post-types/class.creative-slider-cpt.php
+++ plugins/creative-slider/post-types/class.creative-slider-cpt.php
@@ -12,32 +12,32 @@ if ( !class_exists( 'Creative_Slider_Post_Type') ){
 				}
 
 				public function create_post_type(){
-						register_post_type(
-								'creative-slider',
-								array(
-										'label' => 'Slider',
-										'description'   => 'Sliders',
-										'labels' => array(
-												'name'  => 'Sliders',
-												'singular_name' => 'Slider'
-										),
-										'public'    => true,
-										'supports'  => array( 'title', 'editor', 'thumbnail' ),
-										'hierarchical'  => false,
-										'show_ui'   => true,
-										'show_in_menu'  => false,
-										'menu_position' => 5,
-										'show_in_admin_bar' => true,
-										'show_in_nav_menus' => true,
-										'can_export'    => true,
-										'has_archive'   => false,
-										'exclude_from_search'   => false,
-										'publicly_queryable'    => true,
-										'show_in_rest'  => true,
-										'menu_icon' => 'dashicons-images-alt2',
-										//'register_meta_box_cb'  =>  array( $this, 'add_meta_boxes' )
-								)
-						);
+					register_post_type(
+							'creative-slider',
+							array(
+									'label' => 'Slider',
+									'description'   => 'Sliders',
+									'labels' => array(
+											'name'  => 'Sliders',
+											'singular_name' => 'Slider'
+									),
+									'public'    => true,
+									'supports'  => array( 'title', 'editor', 'thumbnail' ),
+									'hierarchical'  => false,
+									'show_ui'   => true,
+									'show_in_menu'  => false,
+									'menu_position' => 5,
+									'show_in_admin_bar' => true,
+									'show_in_nav_menus' => true,
+									'can_export'    => true,
+									'has_archive'   => false,
+									'exclude_from_search'   => false,
+									'publicly_queryable'    => true,
+									'show_in_rest'  => true,
+									'menu_icon' => 'dashicons-images-alt2',
+									//'register_meta_box_cb'  =>  array( $this, 'add_meta_boxes' )
+							)
+					);
 				}
 
 				public function creative_slider_cpt_columns( $columns ) {
