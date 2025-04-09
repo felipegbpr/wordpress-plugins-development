99a2ad09695f246853ac3cf548b42610a89205ef
Felipe Borges <felipetecacc@gmail.com>
2025-04-09 00:47:43 -0300

    feat: Functionality for uninstalling the plugin

--- plugins/creative-slider/creative-slider.php
+++ plugins/creative-slider/creative-slider.php
@@ -73,7 +73,20 @@ if ( ! class_exists( 'Creative_Slider' ) ) {
     }
 
     public static function uninstall() {
+      
+      delete_option( 'creative_slide_options' );
 
+      $posts = get_posts( 
+        array(
+          'post_type' => 'creative-slider',
+          'number_posts'  =>  -1,
+          'post_status'   => 'any'
+        )
+      );
+
+      foreach( $posts as $post ) {
+        wp_delete_post( $post->ID, true );
+      }
     }
 
     public function load_textdomain() {
