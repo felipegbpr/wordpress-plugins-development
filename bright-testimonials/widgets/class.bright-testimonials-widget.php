bc11994ee0bc0de3005e45b0740d837661d7b66f
Felipe Borges <felipetecacc@gmail.com>
2025-04-14 22:45:16 -0300

    impl: widget api - creating the widget form

--- bright-testimonials/widgets/class.bright-testimonials-widget.php
+++ bright-testimonials/widgets/class.bright-testimonials-widget.php
@@ -22,7 +22,46 @@ class Bright_Testimonials_Widget extends WP_Widget {
 	}
 
 	public function form( $instance ) {
+		$title = 'Test title';
+		$number = 5;
+		$image = false;
+		$occupation = true;
+		$company = true;
+		?>
+			<p>
+				<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e( 'Title', 'bright-testimonials' ); ?>:</label>
+				<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" 
+					name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>">
+			</p>
 
+			<p>
+				<label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php esc_html_e( 'Number of testimonials to show', 
+				'bright-testimonials' ); ?>:</label>
+				<input type="number" class="tiny-text" id="<?php echo $this->get_field_id( 'number' ); ?>" 
+					name="<?php echo $this->get_field_name( 'number' ); ?>" step="1" min="1" size="3" value="<?php echo esc_attr( $number ); ?>">
+			</p>
+
+			<p>
+				<input type="checkbox" class="widefat" id="<?php echo $this->get_field_id( 'image' ); ?>" 
+					name="<?php echo $this->get_field_name( 'image' ); ?>" <?php checked( $image ); ?>>
+				<label for="<?php echo $this->get_field_id( 'image' ); ?>"><?php esc_html_e( 'Display user image?', 
+					'bright-testimonials' ); ?></label>
+			</p>
+
+			<p>
+				<input type="checkbox" class="widefat" id="<?php echo $this->get_field_id( 'occupation' ); ?>" 
+					name="<?php echo $this->get_field_name( 'occupation' ); ?>" <?php checked( $occupation ); ?>>
+				<label for="<?php echo $this->get_field_id( 'occupation' ); ?>"><?php esc_html_e( 'Display occupation?', 
+					'bright-testimonials' ); ?></label>
+			</p>
+
+			<p>
+				<input type="checkbox" class="widefat" id="<?php echo $this->get_field_id( 'company' ); ?>" 
+					name="<?php echo $this->get_field_name( 'company' ); ?>"  <?php checked( $company ); ?>>
+				<label for="<?php echo $this->get_field_id( 'company' ); ?>"><?php esc_html_e( 'Display company?', 
+					'bright-testimonials' ); ?></label>
+			</p>
+		<?php 
 	}
 
 	public function widget( $args, $instance ) {
