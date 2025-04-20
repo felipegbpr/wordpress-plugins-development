46e0dfbfe58bdd1d900f816719c234c62059e5c5
Felipe Borges <felipetecacc@gmail.com>
2025-04-20 13:28:46 -0300

    impl: $wpdb Class - Creating your own table

--- inspire-translations/inspire-translations.php
+++ inspire-translations/inspire-translations.php
@@ -7,7 +7,7 @@
  * Version: 1.0
  * Requires at least: 5.6
  * Requires PHP: 7.0
- * Author: Marcelo Vieira
+ * Author: Felipe Borges
  * Author URI: https://www.auhtor.net
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -55,9 +55,33 @@ if (!class_exists('Inspire_Translations')) {
 		/**
 		 * Activate the plugin
 		 */
-		public static function activate()
-		{
-			update_option('rewrite_rules', '');
+		public static function activate() {
+			update_option('rewrite_rules', '' );
+
+			global $wpdb;
+
+			$table_name = $wpdb->prefix . "translationmeta";
+
+			$ipt_db_version = get_option( 'ipt_translation_db_version' ) ;
+
+			if( empty( $ipt_db_version ) ) {
+					$query = "
+							CREATE TABLE $table_name (
+									meta_id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+									translation_id bigint(20) NOT NULL DEFAULT '0',
+									meta_key varchar(255) DEFAULT NULL,
+									meta_value longtext,
+									PRIMARY KEY  (meta_id),
+									KEY translation_id (translation_id),
+									KEY meta_key (meta_key))
+									ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+					
+					require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+					dbDelta( $query );
+
+					$ipt_db_version = '1.0';
+					add_option( 'ipt_translation_db_version', $ipt_db_version );
+			}
 		}
 
 		/**
