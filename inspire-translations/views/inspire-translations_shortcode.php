570e26f389b1d51f1cc0cf5a0533f043ed07cf4d
Felipe Borges <felipetecacc@gmail.com>
2025-04-25 20:59:32 -0300

    feat: Creating the view for the shortcode (part 9)

--- inspire-translations/views/inspire-translations_shortcode.php
+++ inspire-translations/views/inspire-translations_shortcode.php
@@ -57,25 +57,6 @@ if( isset( $_POST['submitted'])){
 ?>		
 
 <div class="inspire-translations">
-<?php 
-
-global $current_user;
-global $wpdb; 
-$q = $wpdb->prepare(
-	"SELECT ID, post_author, post_date, post_title, post_status, meta_key, meta_value
-	FROM $wpdb->posts AS p
-	INNER JOIN $wpdb->translationmeta AS tm
-	ON p.ID = tm.translation_id
-	WHERE p.post_author = %d
-	AND tm.meta_key = 'ipt_translations_transliteration'
-	AND p.post_status IN ( 'publish', 'pending' )
-	ORDER BY p.post_date DESC", 
-	$current_user->ID
-);
-
-$results = $wpdb->get_results( $q );
-var_dump( $results );
-?>
 
 	<form action="" method="POST" id="translations-form">
 		<h2><?php esc_html_e( 'Submit new translation' , 'inspire-translations' ); ?></h2>
@@ -129,6 +110,27 @@ var_dump( $results );
 	</form>
 </div>
 <div class="translations-list">
+<?php 
+
+global $current_user;
+global $wpdb; 
+$q = $wpdb->prepare(
+	"SELECT ID, post_author, post_date, post_title, post_status, meta_key, meta_value
+	FROM $wpdb->posts AS p
+	INNER JOIN $wpdb->translationmeta AS tm
+	ON p.ID = tm.translation_id
+	WHERE p.post_author = %d
+	AND tm.meta_key = 'ipt_translations_transliteration'
+	AND p.post_status IN ( 'publish', 'pending' )
+	ORDER BY p.post_date DESC", 
+	$current_user->ID
+);
+
+$results = $wpdb->get_results( $q );
+
+if ( $wpdb->num_rows ):	
+
+?>
 	<table>
 			<caption><?php esc_html_e( 'Your Translations', 'inspire-translations' ); ?></caption>
 			<thead>
@@ -142,14 +144,22 @@ var_dump( $results );
 				</tr>
 			</thead>  
 			<tbody>  
-				<tr>
-					<td>Date</td>
-					<td>Title</td>
-					<td>Transliteraton</td>
-					<td>Edit</td>
-					<td>Delete</td>
-					<td>Status</td>
-				</tr>
+				<?php foreach( $results as $result ): ?>
+					<tr>
+						<td><?php echo esc_html ( date( 'M/d/Y', strtotime( $result->post_date ) ) );  ?></td>
+						<td><?php echo esc_html( $result->post_title ); ?></td>
+						<td><?php echo $result->meta_value == 'Yes' ? esc_html__( 'Yes', 'inspire-translations' ) : 
+							esc_html__( 'No', 'inspire-translations' ); ?></td>
+						<?php $edit_post = add_query_arg( 'post', $result->ID, home_url( '/edit-translation' ) ); ?>	
+						<td><a href="<?php echo esc_url( $edit_post ); ?>"><?php esc_html_e( 'Edit', 'inspire-translations' ); ?></a>
+						</td>
+						<td><a onclick="return confirm( 'Are you sure you want to delete post: <?php echo $result->post_title ?>?' )" 
+							href="<?php echo get_delete_post_link( $result->ID, "", true ); ?>"><?php esc_html_e( 'Delete', 'inspire-translations' ); ?></a></td>
+						<td><?php echo $result->post_status == 'publish' ? esc_html__( 'Published', 'inspire-translations' ) : 
+							esc_html__( 'Pending', 'inspire-translations' ); ?></td>
+					</tr>
+				<?php endforeach; ?>
 	</tbody>
 </table>
+<?php endif; ?>
 </div>
