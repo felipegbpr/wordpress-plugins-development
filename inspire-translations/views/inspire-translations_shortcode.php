eafbd7ae943c708c61195911449f9dc05335285d
Felipe Borges <felipetecacc@gmail.com>
2025-04-25 14:53:04 -0300

    feat: Creating the view for the shortcode (part 5)

--- inspire-translations/views/inspire-translations_shortcode.php
+++ inspire-translations/views/inspire-translations_shortcode.php
@@ -68,23 +68,32 @@ if( isset( $_POST['submitted'])){
 		?>
 		
 		<label for="ipt_translations_title"><?php esc_html_e( 'Title', 'inspire-translations' ); ?> *</label>
-		<input type="text" name="ipt_translations_title" id="ipt_translations_title" value="" required />
+		<input type="text" name="ipt_translations_title" id="ipt_translations_title" 
+			value="<?php if ( isset( $title ) ) echo $title; ?>" required />
 		<br />
 		<label for="ipt_translations_singer"><?php esc_html_e( 'Singer', 'inspire-translations' ); ?> *</label>
-		<input type="text" name="ipt_translations_singer" id="ipt_translations_singer" value="" required />
+		<input type="text" name="ipt_translations_singer" id="ipt_translations_singer" 
+			value="<?php if ( isset( $singer ) ) echo $singer; ?>" required />
 
 		<br />
-		<?php wp_editor( '', 'ipt_translations_content', array( 'wpautop' => true, 'media_buttons' => false ) ); ?>	
+		<?php 
+			if ( isset( $content ) ) {
+				wp_editor( $content, 'ipt_translations_content', array( 'wpautop' => true, 'media_buttons' => false ) ); 
+			} else {
+				wp_editor( '', 'ipt_translations_content', array( 'wpautop' => true, 'media_buttons' => false ) ); 
+			}
+		?> 	
 		</br />
 		
 		<fieldset id="additional-fields">
 			<label for="ipt_translations_transliteration"><?php esc_html_e( 'Has transliteration?', 'inspire-translations' ); ?></label>
 			<select name="ipt_translations_transliteration" id="ipt_translations_transliteration">
-				<option value="Yes"><?php esc_html_e( 'Yes', 'inspire-translations' ); ?></option>
-				<option value="No"><?php esc_html_e( 'No', 'inspire-translations' ); ?></option>
+				<option value="Yes" <?php if ( isset( $transliteration ) ) selected( $transliteration, "Yes" ); ?>><?php esc_html_e( 'Yes', 'inspire-translations' ); ?></option>
+				<option value="No <?php if ( isset( $transliteration ) ) selected( $transliteration, "No" ); ?>"><?php esc_html_e( 'No', 'inspire-translations' ); ?></option>
 			</select>
 			<label for="ipt_translations_video_url"><?php esc_html_e( 'Video URL', 'inspire-translations' ); ?></label>
-			<input type="url" name="ipt_translations_video_url" id="ipt_translations_video_url" value="" />
+			<input type="url" name="ipt_translations_video_url" id="ipt_translations_video_url" 
+				value="<?php if ( isset( $video ) ) echo $video; ?>" />
 		</fieldset>
 		<br />
 		<input type="hidden" name="ipt_translations_action" value="save">
