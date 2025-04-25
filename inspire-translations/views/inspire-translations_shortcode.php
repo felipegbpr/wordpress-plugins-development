0038f72fc3869ef08c76ca48ea50d4da0e6db254
Felipe Borges <felipetecacc@gmail.com>
2025-04-25 16:40:05 -0300

    feat: Creating the view for the shortcode (part 8)

--- inspire-translations/views/inspire-translations_shortcode.php
+++ inspire-translations/views/inspire-translations_shortcode.php
@@ -57,6 +57,26 @@ if( isset( $_POST['submitted'])){
 ?>		
 
 <div class="inspire-translations">
+<?php 
+
+global $current_user;
+global $wpdb; 
+$q = $wpdb->prepare(
+	"SELECT ID, post_author, post_date, post_title, post_status, meta_key, meta_value
+	FROM $wpdb->posts AS p
+	INNER JOIN $wpdb->translationmeta AS tm
+	ON p.ID = tm.translation_id
+	WHERE p.post_author = %d
+	AND tm.meta_key = 'ipt_translations_transliteration'
+	AND p.post_status IN ( 'publish', 'pending' )
+	ORDER BY p.post_date DESC", 
+	$current_user->ID
+);
+
+$results = $wpdb->get_results( $q );
+var_dump( $results );
+?>
+
 	<form action="" method="POST" id="translations-form">
 		<h2><?php esc_html_e( 'Submit new translation' , 'inspire-translations' ); ?></h2>
 
